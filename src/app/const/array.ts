d75865123f09ef208a5004e92d729442cc7fd3f3
sumit161 <117164245+sumit161@users.noreply.github.com>
2023-04-29 19:41:54 +0530

    change array

--- src/app/const/array.ts
+++ src/app/const/array.ts
@@ -1,5 +1,6 @@
 import { Istudent } from "../model/interface";
 
 export let Students :Array<Istudent>= [
-  {fname:"sumit",lname:"patil",contact:"123",email:"Sumit@gmail",id:"qwe"},
+  {fname:"jhon",lname:"doe",contact:"123",email:"jhon@gmail",id:"qwe"},
+  {fname:"lucy",lname:"doe",contact:"124",email:"lucy@gmail",id:"pqwe"},
 ];
